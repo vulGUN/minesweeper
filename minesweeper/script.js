d4b5c3e293b9691782b7700adcfa692d68151316
Max <likha.max@gmail.com>
2023-05-19 00:49:42 +0300

    feat: add new font; add title; add a game stop when hitting a cell with a mine

--- minesweeper/script.js
+++ minesweeper/script.js
@@ -1,5 +1,6 @@
 const { body } = document;
-body.innerHTML = '<section class="field"></section>';
+body.innerHTML += '<h1 class="title">Minesweeper</h1>';
+body.innerHTML += '<section class="field"></section>';
 const field = document.querySelector('.field');
 
 const level = {
@@ -9,6 +10,7 @@ const level = {
 };
 
 let [boardSize, mineCount] = level.easy;
+const bombIndex = [];
 
 // установка размеров поля в зависимости от сложности
 function setBoardSize(boardSize) {
@@ -39,34 +41,39 @@ function init(boardSize) {
 setBoardSize(boardSize);
 init(boardSize);
 
+const bombs = [];
+
 const buttons = [...document.querySelectorAll('.field__button')];
 buttons.forEach((i, index) => {
-  i.addEventListener('contextmenu', (e) => {
-    e.preventDefault();
-    if (!i.classList.contains('field__button_active')) {
-      i.classList.toggle('flag_active');
-    }
-  });
-
-  i.addEventListener('click', () => {
-    if (!i.classList.contains('flag_active')) {
+  const leftClick = function () {
+    if (!i.classList.contains('flag_active') && !i.classList.contains('game-over')) {
       const row = Math.floor(index / boardSize);
       const col = index % boardSize;
 
       if (bombs[row][col]) {
         i.classList.add('bomb_active', 'field__button_active');
+        lose();
       } else {
         openCell(row, col);
       }
     }
-  });
+  };
 
-  i.addEventListener('dblclick', (e) => {
+  const dblclick = function (e) {
     e.preventDefault();
-  });
-});
+  };
 
-const bombs = [];
+  const rightClick = function (e) {
+    e.preventDefault();
+    if (!i.classList.contains('field__button_active')) {
+      i.classList.toggle('flag_active');
+    }
+  };
+
+  i.addEventListener('click', leftClick);
+  i.addEventListener('dblclick', dblclick);
+  i.addEventListener('contextmenu', rightClick);
+});
 
 generateBombs();
 
@@ -87,7 +94,6 @@ function calculateBombCount(row, col) {
 }
 
 function generateBombs() {
-  const bombIndex = [];
   while (bombIndex.length < mineCount) {
     const randomRow = Math.floor(Math.random() * boardSize);
     const randomCol = Math.floor(Math.random() * boardSize);
@@ -149,9 +155,6 @@ function openCell(row, col) {
   }
 }
 
-window.addEventListener('resize', resizeWidth);
-window.addEventListener('load', resizeWidth);
-
 function resizeWidth() {
   const root = document.documentElement;
   let cellWidth, fieldWidth;
@@ -164,4 +167,21 @@ function resizeWidth() {
   }
   root.style.setProperty('--cell-size', `${cellWidth}px`);
   root.style.setProperty('--field-size', `${fieldWidth}px`);
+}
+
+window.addEventListener('resize', resizeWidth);
+window.addEventListener('load', resizeWidth);
+
+function lose() {
+  for (let i = 0; i < mineCount; i++) {
+    const index = bombIndex[i][0] * boardSize + bombIndex[i][1];
+    const cell = document.querySelectorAll('.field__button')[index];
+    if (!cell.classList.contains('bomb_active')) {
+      cell.classList.add('bomb_inactive', 'field__button_active');
+      cell.classList.remove('flag_active');
+    }
+  }
+  buttons.forEach((i) => {
+    i.classList.add('game-over');
+  });
 }
