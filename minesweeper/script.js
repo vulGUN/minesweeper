b4ff70b70c30c5d8556e0efb94b5932d6d6e28e9
Max <likha.max@gmail.com>
2023-05-18 18:01:59 +0300

    feat: add dynamic resizing of the playing field relative to the screen; add a display of the number of bombs in the cell; add opening adjacent cells if they are empty

--- minesweeper/script.js
+++ minesweeper/script.js
@@ -10,6 +10,7 @@ const level = {
 
 let [boardSize, mineCount] = level.easy;
 
+// установка размеров поля в зависимости от сложности
 function setBoardSize(boardSize) {
   switch (boardSize) {
     case 10:
@@ -33,56 +34,130 @@ function init(boardSize) {
     cell.classList.add('field__button');
     field.appendChild(cell);
   }
-
-  let buttons = [...document.querySelectorAll('.field__button')];
-  buttons.forEach((i, index) => {
-    i.addEventListener('click', (e) => {
-      const row = Math.floor(index / boardSize);
-      const col = index % boardSize;
-
-      e.target.classList.add('field__button_active');
-      isBomb(index, indexBombs, i);
-      console.log(+`${row}${col}`);
-    });
-  });
-
-  const indexBombs = generateBomb();
-  console.log(indexBombs);
 }
 
 setBoardSize(boardSize);
 init(boardSize);
 
-function generateBomb() {
-  const bombArr = [];
-  for (let i = 0; i < mineCount; i++) {
-    const randomNum = Math.floor(Math.random() * boardSize ** 2);
-    if (bombArr.includes(randomNum)) i -= 1;
-    else bombArr.push(randomNum);
+const buttons = [...document.querySelectorAll('.field__button')];
+buttons.forEach((i, index) => {
+  i.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    if (!i.classList.contains('field__button_active')) {
+      i.classList.toggle('flag_active');
+    }
+  });
+
+  i.addEventListener('click', () => {
+    if (!i.classList.contains('flag_active')) {
+      const row = Math.floor(index / boardSize);
+      const col = index % boardSize;
+
+      if (bombs[row][col]) {
+        i.classList.add('bomb_active', 'field__button_active');
+      } else {
+        openCell(row, col);
+      }
+    }
+  });
+});
+
+const bombs = [];
+
+generateBombs();
+
+function calculateBombCount(row, col) {
+  let bombCount = 0;
+
+  for (let i = row - 1; i <= row + 1; i++) {
+    for (let j = col - 1; j <= col + 1; j++) {
+      if (i >= 0 && i < boardSize && j >= 0 && j < boardSize) {
+        if (bombs[i][j]) {
+          bombCount++;
+        }
+      }
+    }
   }
-  return bombArr;
+
+  return bombCount;
 }
 
-function isBomb(index, indexBomb, el) {
-  if (indexBomb.includes(index)) {
-    el.classList.add('bomb_active');
+function generateBombs() {
+  const bombIndex = [];
+  while (bombIndex.length < mineCount) {
+    const randomRow = Math.floor(Math.random() * boardSize);
+    const randomCol = Math.floor(Math.random() * boardSize);
+    const newBomb = [randomRow, randomCol];
+    if (!bombIndex.some((bomb) => bomb[0] === randomRow && bomb[1] === randomCol)) {
+      bombIndex.push(newBomb);
+    }
+  }
+  for (let i = 0; i < boardSize; i++) {
+    bombs[i] = [];
+    for (let j = 0; j < boardSize; j++) {
+      bombs[i][j] = false;
+    }
+  }
+  bombIndex.forEach((j) => {
+    const [row, col] = j;
+    bombs[row][col] = true;
+  });
+}
+
+function openCell(row, col) {
+  const cellIndex = row * boardSize + col;
+  const cell = document.querySelectorAll('.field__button')[cellIndex];
+
+  if (cell.classList.contains('field__button_active')) {
+    return;
+  }
+
+  const bombCount = calculateBombCount(row, col);
+  if (bombCount > 0) cell.textContent = bombCount;
+  switch (cell.textContent) {
+    case '1':
+      cell.style.color = 'blue';
+      break;
+    case '2':
+      cell.style.color = 'green';
+      break;
+    case '3':
+      cell.style.color = 'red';
+      break;
+    case '4':
+      cell.style.color = 'darkblue';
+      break;
+    case '5':
+      cell.style.color = 'darkred';
+      break;
+  }
+
+  cell.classList.add('field__button_active');
+
+  if (bombCount === 0) {
+    for (let i = row - 1; i <= row + 1; i++) {
+      for (let j = col - 1; j <= col + 1; j++) {
+        if (i >= 0 && i < boardSize && j >= 0 && j < boardSize) {
+          openCell(i, j);
+        }
+      }
+    }
   }
 }
 
-// switch (el.textContent) {
-//   case '1':
-//     el.style.color = 'blue';
-//     break;
-//   case '2':
-//     el.style.color = 'green';
-//     break;
-//   case '3':
-//     el.style.color = 'red';
-//     break;
-//   case '4':
-//     el.style.color = 'darkblue';
-//     break;
-//   case '5':
-//     el.style.color = 'orange';
-//     break;
-// }
+window.addEventListener('resize', resizeWidth);
+window.addEventListener('load', resizeWidth);
+
+function resizeWidth() {
+  const root = document.documentElement;
+  let cellWidth, fieldWidth;
+  if (window.innerWidth >= 430) {
+    cellWidth = 400 / boardSize;
+    fieldWidth = boardSize * cellWidth;
+  } else {
+    cellWidth = (window.innerWidth - 32) / boardSize;
+    fieldWidth = window.innerWidth - 32;
+  }
+  root.style.setProperty('--cell-size', `${cellWidth}px`);
+  root.style.setProperty('--field-size', `${fieldWidth}px`);
+}
