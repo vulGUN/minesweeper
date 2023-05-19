1cab346f52ea09ff6aada684f98cf3f8cd69a93d
Max <likha.max@gmail.com>
2023-05-19 15:57:04 +0300

    feat: add sounds for the game; add text display when winning and losing; add a step counter; add game time counting function

--- minesweeper/script.js
+++ minesweeper/script.js
@@ -11,6 +11,14 @@ const level = {
 
 let [boardSize, mineCount] = level.easy;
 const bombIndex = [];
+const bombs = [];
+let stepCounter = 0;
+let closeCellCount = boardSize ** 2;
+let endGame = false;
+let playTime = 1;
+let endTime = 0;
+let timer;
+let timerActive = false;
 
 // установка размеров поля в зависимости от сложности
 function setBoardSize(boardSize) {
@@ -37,45 +45,6 @@ function init(boardSize) {
     field.appendChild(cell);
   }
 }
-
-setBoardSize(boardSize);
-init(boardSize);
-
-const bombs = [];
-
-const buttons = [...document.querySelectorAll('.field__button')];
-buttons.forEach((i, index) => {
-  const leftClick = function () {
-    if (!i.classList.contains('flag_active') && !i.classList.contains('game-over')) {
-      const row = Math.floor(index / boardSize);
-      const col = index % boardSize;
-
-      if (bombs[row][col]) {
-        i.classList.add('bomb_active', 'field__button_active');
-        lose();
-      } else {
-        openCell(row, col);
-      }
-    }
-  };
-
-  const dblclick = function (e) {
-    e.preventDefault();
-  };
-
-  const rightClick = function (e) {
-    e.preventDefault();
-    if (!i.classList.contains('field__button_active')) {
-      i.classList.toggle('flag_active');
-    }
-  };
-
-  i.addEventListener('click', leftClick);
-  i.addEventListener('dblclick', dblclick);
-  i.addEventListener('contextmenu', rightClick);
-});
-
-generateBombs();
 
 function calculateBombCount(row, col) {
   let bombCount = 0;
@@ -143,6 +112,7 @@ function openCell(row, col) {
   }
 
   cell.classList.add('field__button_active');
+  closeCellCount--;
 
   if (bombCount === 0) {
     for (let i = row - 1; i <= row + 1; i++) {
@@ -169,9 +139,6 @@ function resizeWidth() {
   root.style.setProperty('--field-size', `${fieldWidth}px`);
 }
 
-window.addEventListener('resize', resizeWidth);
-window.addEventListener('load', resizeWidth);
-
 function lose() {
   for (let i = 0; i < mineCount; i++) {
     const index = bombIndex[i][0] * boardSize + bombIndex[i][1];
@@ -181,7 +148,102 @@ function lose() {
       cell.classList.remove('flag_active');
     }
   }
-  buttons.forEach((i) => {
-    i.classList.add('game-over');
-  });
+  endGame = true;
+  const html = `<div class="end-title">Game over. Try again</div>`;
+  body.insertAdjacentHTML('afterend', html);
+  clearInterval(timer);
+  playLoseAudio();
 }
+
+function win() {
+  if (closeCellCount <= mineCount && endGame === false) {
+    endGame = true;
+    const html = `<div class="end-title">Hooray! You found all mines in ${playTime} seconds and ${stepCounter} moves!</div>`;
+    body.insertAdjacentHTML('afterend', html);
+    clearInterval(timer);
+    playWinAudio();
+  }
+}
+
+function timeInterval() {
+  let sec = Math.round(playTime) % 60,
+    min = Math.floor(playTime / 60);
+  endTime = `${min.toString().padStart(2, '0')}:${sec.toString().padStart(2, '0')}`;
+
+  if (playTime < 3599) playTime++;
+}
+
+function playWinAudio() {
+  const audio = new Audio('./assets/audio/win.mp3');
+  audio.play();
+}
+
+function playLoseAudio() {
+  const audioLose = new Audio('./assets/audio/lose.mp3');
+  const audioCrash = new Audio('./assets/audio/crash.mp3');
+  audioCrash.play();
+  audioLose.play();
+}
+
+function playSnapAudio() {
+  const audio = new Audio('./assets/audio/snap.mp3');
+  audio.play();
+}
+
+function playFlagAudio() {
+  const audio = new Audio('./assets/audio/flag.mp3');
+  audio.play();
+}
+
+setBoardSize(boardSize);
+init(boardSize);
+
+const buttons = [...document.querySelectorAll('.field__button')];
+buttons.forEach((i, index) => {
+  const leftClick = function () {
+    if (!i.classList.contains('flag_active') && !endGame) {
+      const row = Math.floor(index / boardSize);
+      const col = index % boardSize;
+
+      if (!timerActive) {
+        timer = setInterval(timeInterval, 1000);
+        timerActive = true;
+      }
+
+      if (bombs[row][col]) {
+        i.classList.add('bomb_active', 'field__button_active');
+        lose();
+      } else {
+        openCell(row, col);
+      }
+    }
+  };
+
+  const dblclick = function (e) {
+    e.preventDefault();
+  };
+
+  const rightClick = function (e) {
+    e.preventDefault();
+    if (!i.classList.contains('field__button_active') && endGame === false) {
+      i.classList.toggle('flag_active');
+      playFlagAudio();
+    }
+  };
+
+  i.addEventListener('click', () => {
+    if (!i.classList.contains('field__button_active') && endGame === false) {
+      ++stepCounter;
+      i.classList.contains('flag_active') ? null : playSnapAudio();
+    }
+    leftClick();
+    win();
+  });
+  i.addEventListener('dblclick', dblclick);
+  i.addEventListener('contextmenu', rightClick);
+});
+
+generateBombs();
+
+window.addEventListener('resize', resizeWidth);
+window.addEventListener('load', resizeWidth);
